58483277c38dfc4b7123bf0a86f41ff3021f7937
derek <derekle.creative@gmail.com>
2021-08-14 18:33:08 -0500

    commit tree 33ea54a8b7266101c217d0ee7458e71bf7bd7d29

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+function mapToNegativize(a){
+    console.log(a);
+    return a.map(x => x * -1);
+}
+
+function mapToNoChange(a){
+    return a;
+}
+
+function mapToDouble(a){
+    return a.map(x => x * 2);
+}
+
+function mapToSquare(a){
+    return a.map(x => x*x);
+}
+
+
+function reduceToTotal(a, int){
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+    if (!!int) {
+        return a.reduce(reducer,int)
+    } else {
+        return a.reduce(reducer)
+    }
+}
+
+function reduceToAllTrue(a){
+    if (a.filter(val => !val)[0] == false) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function reduceToAnyTrue(a){
+    if (a.includes(true)) {
+        return true
+    } else {
+        return false
+    }
+}
